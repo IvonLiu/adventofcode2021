9ab5c407ccbf3b8801b36267db32adb100482be8
Ivon Liu <me@ivonliu.com>
2021-12-06 01:21:04 -0500

    Day 1 part 2

--- day1/solution.py
+++ day1/solution.py
@@ -1,9 +1,10 @@
 import argparse
+from collections import deque
 
 def part1(input_path):
     increased_count = 0
     prev = None
-    with open (input_path, "r") as f:
+    with open(input_path, "r") as f:
         for line in f:
             curr = int(line)
             if prev is not None:
@@ -13,8 +14,22 @@ def part1(input_path):
     print(increased_count)
 
 
+def part2(input_path):
+    increased_count = 0
+    queue = deque()
+    with open(input_path, "r") as f:
+        for line in f:
+            curr = int(line)
+            if len(queue) == 3:
+                if curr > queue.popleft():
+                    increased_count += 1
+            queue.append(curr)
+    print(increased_count)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input-path", type=str, required=True)
+    parser.add_argument("fn", type=str)
     args = parser.parse_args()
-    part1(args.input_path)
+    globals()[args.fn](args.input_path)
