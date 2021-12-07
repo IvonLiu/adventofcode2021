3fe8175648de523a90d6c1ceb384c2ccc1b98da8
Ivon Liu <me@ivonliu.com>
2021-12-07 01:34:47 -0500

    Day 2 part 2

--- day2/solution.py
+++ day2/solution.py
@@ -1,22 +1,39 @@
 import argparse
 
-def part1(input_path):
-    h_pos = 0
-    depth = 0
+def _process_lines(input_path):
     with open(input_path) as f:
         for line in f:
             direction, val = line.rstrip().split(" ")
             val = int(val)
-            if direction == "forward":
-                h_pos += val
-            elif direction == "down":
-                depth += val
-            elif direction == "up":
-                depth -= val
+            yield direction, val
+
+def part1(input_path):
+    h_pos = 0
+    depth = 0
+    for direction, val in _process_lines(input_path):
+        if direction == "forward":
+            h_pos += val
+        elif direction == "down":
+            depth += val
+        elif direction == "up":
+            depth -= val
     print(h_pos * depth)
 
+
 def part2(input_path):
-    pass
+    h_pos = 0
+    depth = 0
+    aim = 0
+    for direction, val in _process_lines(input_path):
+        if direction == "forward":
+            h_pos += val
+            depth += val * aim
+        elif direction == "down":
+            aim += val
+        elif direction == "up":
+            aim -= val
+    print(h_pos * depth)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
