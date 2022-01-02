ef6175923beb66196d10c58ca2efefe46d8ec8d7
Ivon Liu <me@ivonliu.com>
2022-01-02 03:46:19 -0500

    Day 5 part 2

--- day5/solution.py
+++ day5/solution.py
@@ -15,14 +15,29 @@ def _parse_input(input_path, ignore_diagonal=True):
 
 
 def _sign(n):
-    assert n != 0
+    if n == 0:
+        return 0
     return n / abs(n)
 
 
-def part1(input_path):
+def _traverse(start, end, check):
+    d = (
+        _sign(end[0] - start[0]),
+        _sign(end[1] - start[1]),
+    )
+
+    x, y = start
+    while x != end[0] or y != end[1]:
+        check(x, y)
+        x += d[0]
+        y += d[1]
+    check(*end)
+
+
+def _solve(input_path, ignore_diagonal):
     points = set()
     danger = set()
-    lines = _parse_input(input_path)
+    lines = _parse_input(input_path, ignore_diagonal=ignore_diagonal)
 
     def check(x, y):
         if (x, y) in danger:
@@ -33,25 +48,17 @@ def part1(input_path):
             points.add((x, y))
 
     for start, end in lines:
-        if start[0] == end[0]:
-            d = (0, _sign(end[1] - start[1]))
-        elif start[1] == end[1]:
-            d = (_sign(end[0] - start[0]), 0)
-        else:
-            raise ValueError(f"({start}, {end}) is a diagonal")
-
-        x, y = start
-        while x != end[0] or y != end[1]:
-            check(x, y)
-            x += d[0]
-            y += d[1]
-        check(*end)
+        _traverse(start, end, check)
 
     print(len(danger))
 
 
+def part1(input_path):
+    _solve(input_path, ignore_diagonal=True)
+
+
 def part2(input_path):
-    pass
+    _solve(input_path, ignore_diagonal=False)
 
 
 if __name__ == "__main__":
